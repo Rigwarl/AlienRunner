2ba0a316d91fc0c69c243910b892a4ccee8a0183
Rigwarl <Avips@mail.ru>
2016-09-07 17:00:42 +0300

    enable touch or mouseover

--- app/js/src/app.js
+++ app/js/src/app.js
@@ -7,5 +7,10 @@ screensManager.init(stage);
 assetsManager.load(() => {
   screensManager.change('StartScreen');
   createjs.Sound.play('back', { loop: -1, volume: 0.3 });
+  if (createjs.Touch.isSupported()) {
+    createjs.Touch.enable(stage, true);
+  } else {
+    stage.enableMouseOver(20);
+  }
 });
 
