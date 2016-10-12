ff060d41ebac2ec441022d12dd93ebe677190bb8
Rigwarl <Avips@mail.ru>
2016-10-12 15:16:02 +0300

    move manifest to out const

--- app/js/src/managers/assetsManager.js
+++ app/js/src/managers/assetsManager.js
@@ -1,22 +1,25 @@
+const manifest = [
+  { id: 'monster', src: 'img/monster-sprite.png' },
+  { id: 'bird', src: 'img/bird-sprite.png' },
+  { id: 'chicken', src: 'img/chicken-sprite.png' },
+  { id: 'spike', src: 'img/spike.png' },
+  { id: 'sky', src: 'img/bg/sky.png' },
+  { id: 'start', src: 'img/bg/start.png' },
+  { id: 'mountain', src: 'img/bg/mountain.png' },
+  { id: 'ground', src: 'img/bg/ground.png' },
+  { id: 'btn', src: 'img/btn-sprite.png' },
+  { id: 'back', src: 'sound/background.ogg' },
+  { id: 'flap', src: 'sound/flap.ogg' },
+  { id: 'loose', src: 'sound/loose.ogg' },
+];
+
+
 const assetsManager = {
   load(callback) {
     createjs.Sound.alternateExtensions = ['mp3'];
     this.queue = new createjs.LoadQueue();
     this.queue.installPlugin(createjs.Sound);
-    this.queue.loadManifest([
-      { id: 'monster', src: 'img/monster-sprite.png' },
-      { id: 'bird', src: 'img/bird-sprite.png' },
-      { id: 'chicken', src: 'img/chicken-sprite.png' },
-      { id: 'spike', src: 'img/spike.png' },
-      { id: 'sky', src: 'img/bg/sky.png' },
-      { id: 'start', src: 'img/bg/start.png' },
-      { id: 'mountain', src: 'img/bg/mountain.png' },
-      { id: 'ground', src: 'img/bg/ground.png' },
-      { id: 'btn', src: 'img/btn-sprite.png' },
-      { id: 'back', src: 'sound/background.ogg' },
-      { id: 'flap', src: 'sound/flap.ogg' },
-      { id: 'loose', src: 'sound/loose.ogg' },
-    ]);
+    this.queue.loadManifest(manifest);
     this.queue.addEventListener('complete', callback);
   },
   getResult(name) {
