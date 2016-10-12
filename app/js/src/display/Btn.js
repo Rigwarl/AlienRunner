25fa33bad0b8cbb3bd181a61ffd9c6c1f049016f
Rigwarl <Avips@mail.ru>
2016-10-12 16:26:16 +0300

    move some code

--- app/js/src/display/Btn.js
+++ app/js/src/display/Btn.js
@@ -6,25 +6,26 @@ export default class Btn extends createjs.Container {
 
     this.enabled = true;
 
+    this.createBg();
+    this.createLabel(text);
+
+    this.addEventListener('click', () => this.enabled && createjs.Sound.play('flap'));
+  }
+  createBg() {
     this.bg = new createjs.Sprite(assetsManager.getSpriteSheet('btn'));
     this.bg.regX = this.bg.getBounds().width / 2;
     this.bg.regY = this.bg.getBounds().height / 2;
-
     this.helper = new createjs.ButtonHelper(this.bg);
-
+    this.addChild(this.bg);
+  }
+  createLabel(text) {
     this.label = new createjs.Text(text, '30px CarterOne', '#fff');
     this.label.shadow = new createjs.Shadow('#000', 0, 1, 5);
     this.label.textAlign = 'center';
     this.label.textBaseline = 'middle';
     this.label.mouseEnabled = false;
     this.label.y = -2;
-
-    this.addEventListener('click', () => {
-      if (this.enabled) {
-        createjs.Sound.play('flap');
-      }
-    });
-    this.addChild(this.bg, this.label);
+    this.addChild(this.label);
   }
   disable() {
     this.bg.gotoAndStop('disable');
