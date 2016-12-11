1babfa8366b62fbddf1e756cfcd9eebf7edac354
Rigwarl <Avips@mail.ru>
2016-12-11 22:58:53 +0300

    rewrite init

--- app/js/src/app.js
+++ app/js/src/app.js
@@ -1,21 +1,33 @@
 import screensManager from './managers/screensManager';
 import assetsManager from './managers/assetsManager';
+import serverManager from './managers/serverManager';
 import soundManager from './managers/soundManager';
+import dataManager from './managers/dataManager';
 
 const stage = new createjs.Stage('game-stage');
-
 screensManager.init(stage);
-assetsManager.load(() => {
-  soundManager.init(true);
-  screensManager.change('StartScreen');
 
-  if (createjs.Touch.isSupported()) {
-    createjs.Touch.enable(stage, true);
-  } else {
-    stage.enableMouseOver(20);
-  }
+let server = 'local';
 
-  if (window !== window.parent) {
-    window.addEventListener('click', () => window.focus());
+if (window !== window.parent) {
+  if (document.referrer.includes('vk.com')) {
+    server = 'vk';
   }
-});
+  // createjs stage click dosnt trigger window.focus
+  window.addEventListener('click', () => window.focus());
+}
+
+Promise.all([
+  assetsManager.init(),
+  serverManager.init(server)
+    .then(() => serverManager.get('maxScore'))
+    .then(r => dataManager.init(r || 0)),
+]).then(() => serverManager.get('sound'))
+  .then(r => soundManager.init(r || false))
+  .then(() => screensManager.change('StartScreen'));
+
+if (createjs.Touch.isSupported()) {
+  createjs.Touch.enable(stage, true);
+} else {
+  stage.enableMouseOver(20);
+}
