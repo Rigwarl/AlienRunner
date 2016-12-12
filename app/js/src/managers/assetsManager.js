11b5f8f13dc7032da035e18b97be2426974ddc2f
Rigwarl <Avips@mail.ru>
2016-12-13 00:21:45 +0300

    add reject to assetsManager init method

--- app/js/src/managers/assetsManager.js
+++ app/js/src/managers/assetsManager.js
@@ -80,8 +80,10 @@ const assetsManager = {
     this.queue.installPlugin(createjs.Sound);
     this.queue.loadManifest(manifest);
 
-    return new Promise(resolve =>
-      this.queue.addEventListener('complete', () => resolve()));
+    return new Promise((resolve, reject) => {
+      this.queue.addEventListener('complete', () => resolve());
+      this.queue.addEventListener('error', () => reject());
+    });
   },
   getResult(name) {
     return this.queue.getResult(name);
