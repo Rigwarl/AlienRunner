f8a747e1529f071fa58cf6f483bf1b77efd8bc96
Rigwarl <Avips@mail.ru>
2016-12-13 15:03:10 +0300

    fix reject on empty string

--- app/js/src/managers/serverManager.js
+++ app/js/src/managers/serverManager.js
@@ -16,9 +16,17 @@ const serverManager = {
   get(key) {
     return new Promise(resolve => {
       if (this.server === 'vk') {
-        VK.api('storage.get', { key }, r => resolve(r.response));
+        VK.api('storage.get', { key }, resolve);
       }
-    }).then(JSON.parse);
+    }).then(r => {
+      if (r.error) {
+        throw new Error(r.error);
+      } else if (r.response === '') {
+        // cant JSON.parse empty string but need to get default value
+        return '';
+      }
+      return JSON.parse(r.response);
+    });
   },
   set(key, value) {
     if (this.server === 'vk') {
