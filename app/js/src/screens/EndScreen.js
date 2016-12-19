1111113a41e0020538eae47c7568f5f121336e0d
Rigwarl <Avips@mail.ru>
2016-12-19 12:06:37 +0300

    always show share

--- app/js/src/screens/EndScreen.js
+++ app/js/src/screens/EndScreen.js
@@ -12,30 +12,26 @@ export default class EndScreen extends createjs.Container {
 
     this.bg = new createjs.Bitmap(assetsManager.getResult('start'));
     this.score = new createjs.Text(`Результат: ${dataManager.score} м\n\nРекорд: ${dataManager.maxScore} м`, '40px Guerilla', '#000');
-    this.score.x = width / 2;
     this.score.textAlign = 'center';
+    this.score.x = width / 2;
     this.score.y = 125;
 
-
     this.replayBtn = new Btn('Еще раз');
-    // this.menuBtn = new Btn('Menu', 'orange');
     this.replayBtn.x = width / 2;
-    // this.menuBtn.y = 470;
-    this.replayBtn.y = 390;
+    this.replayBtn.y = 450;
 
-    this.addChild(this.bg, this.score, this.replayBtn);
+    this.shareBtn = new Btn('Поделиться', 'orange');
+    this.shareBtn.x = width / 2;
+    this.shareBtn.y = 350;
+    this.shareBtn.addEventListener('click', () => serverManager.share(dataManager.score));
+
+    this.addChild(this.bg, this.score, this.replayBtn, this.shareBtn);
 
     if (dataManager.score > dataManager.maxScore) {
       dataManager.maxScore = dataManager.score;
       serverManager.set('maxScore', dataManager.maxScore);
       this.score.text = `Новый рекорд: ${dataManager.maxScore} м!`;
-      // this.score.y += 60;
-      this.shareBtn = new Btn('Поделиться', 'orange');
-      this.shareBtn.x = width / 2;
-      this.shareBtn.y = 290;
-      this.addChild(this.shareBtn);
-
-      this.shareBtn.addEventListener('click', () => serverManager.share(dataManager.score));
+      this.score.y += 35;
     }
 
     const soundBtn = new IconBtn(soundManager.isEnabled() ? 'sound' : 'soundOff');
@@ -53,7 +49,6 @@ export default class EndScreen extends createjs.Container {
   }
   bindEvents() {
     this.replayBtn.addEventListener('click', () => screensManager.change('MainScreen'));
-    // this.menuBtn.addEventListener('click', () => screensManager.change('StartScreen'));
 
     this.onKeyDown = e => {
       if (e.keyCode === 32) {
