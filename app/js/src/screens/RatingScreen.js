c0968894590f4f817f2a4a7d78ec4e45cbb83b92
Rigwarl <Avips@mail.ru>
2016-12-22 12:46:35 +0300

    add recall rating for old users

--- app/js/src/screens/RatingScreen.js
+++ app/js/src/screens/RatingScreen.js
@@ -19,22 +19,23 @@ export default class RatingScreen extends createjs.Container {
 
     this.addChild(this.bg, this.gui, this.title);
 
-    serverManager.get('ratingTable', 1).then(
-      r => this.showRating(r),
-      () => {
+    serverManager.get('ratingTable', 1)
+      // todo: remove later, now it add records for old users
+      .then(recalcRatingTable)
+      .then(r => this.showRating(r))
+      .catch(() => {
         const text = new createjs.Text('Рейтинг временно недоступен :(', '25px Guerilla', '#000');
         text.textAlign = 'center';
         text.x = this.width / 2;
         text.y = 150;
         this.addChild(text);
-      }
-    );
+      });
   }
   showRating(ratingTable) {
     let winner = false;
 
     ratingTable.forEach((el, i) => {
-      const text = new createjs.Text(`${i + 1} ${el.name} ${el.score}`, '25px Guerilla', '#000');
+      const text = new createjs.Text(`${i + 1} ${el.name} ${el.score} м`, '25px Guerilla', '#000');
       text.y = 120 + i * 40;
       text.x = 120;
       this.addChild(text);
@@ -46,14 +47,35 @@ export default class RatingScreen extends createjs.Container {
     });
 
     if (!winner) {
-      const text = new createjs.Text(`- ${dataManager.user.name} ${dataManager.maxScore}`, '25px Guerilla', '#7ECE2E');
+      const text = new createjs.Text(`- ${dataManager.user.name} ${dataManager.maxScore} м`, '25px Guerilla', '#7ECE2E');
       text.y = 120 + ratingTable.length * 40;
       text.x = 120;
       this.addChild(text);
-
-      if (dataManager.maxScore > ratingTable[ratingTable.length - 1].score) {
-        text.text = 'Для отображения в рейтинге побейте свой рекорд';
-      }
     }
   }
+}
+
+function recalcRatingTable(ratingTable) {
+  if (ratingTable[ratingTable.length - 1].score < dataManager.maxScore) {
+    const userRating = ratingTable.find(el => el.id === dataManager.user.id);
+
+    if (userRating) {
+      userRating.score = dataManager.maxScore;
+    } else {
+      const newRating = {
+        id: dataManager.user.id,
+        name: dataManager.user.name,
+        score: dataManager.maxScore,
+      };
+      if (ratingTable.length < 10) {
+        ratingTable.push(newRating);
+      } else {
+        ratingTable[ratingTable.length - 1] = newRating;
+      }
+    }
+
+    ratingTable.sort((a, b) => b.score - a.score);
+    serverManager.set('ratingTable', ratingTable, 1);
+  }
+  return ratingTable;
 }
