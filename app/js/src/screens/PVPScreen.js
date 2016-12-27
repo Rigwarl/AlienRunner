07efea743e8dac463091ceea2b748ffc5234424e
Rigwarl <Avips@mail.ru>
2016-12-27 18:23:25 +0300

    add timer on  start

--- app/js/src/screens/PVPScreen.js
+++ app/js/src/screens/PVPScreen.js
@@ -4,6 +4,7 @@ import dataManager from '../managers/dataManager';
 import Background from '../display/Background';
 import Hero from '../display/Hero';
 import Spike from '../display/Spike';
+import Btn from '../display/Btn';
 
 const GROUND_HEIGHT = 82;
 const START_SPEED = 5;
@@ -12,42 +13,77 @@ export default class MainScreen extends createjs.Container {
   constructor(width, height) {
     super();
 
-    dataManager.pvp.pos = Math.floor(Math.random() * 2);
-    const recordIndex = !!dataManager.pvp.pos ? 'pvp0' : 'pvp100';
-    serverManager.get(recordIndex, 1).then(r => this.init(r));
-
     this.width = width;
     this.height = height;
 
     this.speed = START_SPEED;
     this.started = false;
 
-    dataManager.gameType = 'pvp';
-    dataManager.pvp.win = false;
+    this.createBg();
+
+    const watingText = new createjs.Text('Идет подбор соперника', '35px Guerilla', '#000');
+    watingText.textAlign = 'center';
+    watingText.x = width / 2;
+    watingText.y = 170;
+
+    const cancelBtn = new Btn('Отмена', 'orange');
+    cancelBtn.x = width / 2;
+    cancelBtn.y = 340;
+    cancelBtn.addEventListener('click', () => screensManager.change('StartScreen'));
+
+    this.addChild(watingText, cancelBtn);
+
+    dataManager.pvp.pos = Math.floor(Math.random() * 2);
+    const recordIndex = dataManager.pvp.pos ? 'pvp0' : 'pvp100';
+
+    Promise.all([
+      serverManager.get(recordIndex, 1).then(r => this.initData(r)),
+      new Promise(resolve => setTimeout(resolve, Math.random() * 2000 + 500)),
+    ]).then(() => {
+      this.init();
+      this.removeChild(watingText, cancelBtn);
+    });
+
+    this.bindEvents();
   }
-  init(record) {
+  initData(record) {
     this.record = record;
     if (dataManager.user.id === record.user.id) {
       record.user.name = 'Призрачный птиц';
     }
-
+    dataManager.gameType = 'pvp';
+    dataManager.pvp.win = false;
     dataManager.pvp.enemy = record.user;
     dataManager.pvp.spikes = [...record.spikes];
     dataManager.pvp.actions = {};
-
+  }
+  init() {
     this.spikeIndex = 0;
     this.step = 0;
     this.distance = 0;
-    this.started = true;
 
-    this.createBg();
     this.createSpikes();
     this.createHud();
 
-    this.enemy = this.createHero(dataManager.pvp.pos);
-    this.hero = this.createHero(1 - dataManager.pvp.pos);
+    const counter = new createjs.Text(3, '115px Guerilla', '#000');
+    counter.textAlign = 'center';
+    counter.x = this.width / 2;
+    counter.y = 310;
 
-    this.bindEvents();
+    this.addChild(counter);
+
+    const interval = setInterval(() => {
+      counter.text -= 1;
+      if (counter.text < 0) {
+        this.removeChild(counter);
+        this.started = true;
+        clearInterval(interval);
+      }
+    }, 1000);
+
+    this.hero = this.createHero(1 - dataManager.pvp.pos, dataManager.user.name);
+    this.enemy = this.createHero(dataManager.pvp.pos, this.record.user.name);
+    this.enemy.alpha = 0.6;
   }
   createBg() {
     this.bgSky = new Background('sky', this.width);
@@ -63,11 +99,19 @@ export default class MainScreen extends createjs.Container {
     this.spikes.forEach(spike => this.resetSpike(spike));
     this.addChild(...this.spikes);
   }
-  createHero(pos) {
+  createHero(pos, name) {
     const hero = new Hero(dataManager.heroType);
     hero.x = this.width / 2 - 180 * pos;
-    hero.y = 190;
-    this.addChild(hero);
+    hero.y = 190 - 50 * pos;
+
+    const heroName = new createjs.Text(name, '25px Guerilla', '#000');
+    heroName.textAlign = 'center';
+    heroName.y = hero.y - 100;
+    heroName.x = hero.x;
+    this.addChild(hero, heroName);
+
+    createjs.Tween.get(heroName).wait(2400).to({ alpha: 0 }, 800)
+      .call(() => this.removeChild(heroName));
 
     return hero;
   }
@@ -110,6 +154,9 @@ export default class MainScreen extends createjs.Container {
     window.addEventListener('keydown', this.onKeyDown);
   }
   handleAction() {
+    if (!this.started) {
+      return;
+    }
     this.hero.flap();
     dataManager.pvp.actions[this.step] = true;
   }
